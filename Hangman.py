6e8c4bee2dd3b583d1e164ff3e63d698197680ff
hosseinrezaie0 <hossein.rezaie81@gmail.com>
2023-07-27 16:34:48 +0330

    random word selected

--- Hangman.py
+++ Hangman.py
@@ -2,3 +2,18 @@
 import random
 from HangmanWords import words_list
 from HangmanLogo import *
+
+
+#print hangman logo
+print(logo)
+print(status[0])
+print()
+
+
+
+#choose a word from words_list randomly
+chosen_word = random.choice(words_list)
+for i in range(0, len(chosen_word)):
+    print("_", end=" ")
+
+
